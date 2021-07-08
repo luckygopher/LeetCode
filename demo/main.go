114785f3dea666dd4f4f6ff664c8b0bda75dceb6
qingyunjun <mr.arvin.hou@gmail.com>
2021-07-08 21:46:42 +0800

    demo

---
+++ demo/main.go
@@ -0,0 +1,23 @@
+package main
+
+// 用户信息数据结构
+type User struct {
+	// 用户id
+	Id uint64
+	// 父母是否参与火星研究
+	ParentMarsResearch bool
+	// 总年纳税
+	AnnualTax int64
+	// 是否有犯罪记录
+	CriminalRecord bool
+	// 是否有亲兄弟姐妹
+	BrothersAndSisters bool
+	// 是否工程师
+	Engineer bool
+	// 报名时间
+	Time uint64
+}
+
+func main() {
+
+}
